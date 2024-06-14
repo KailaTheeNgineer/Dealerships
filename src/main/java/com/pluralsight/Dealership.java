628e208439df257dd65e019568157d8eb4d53ad1
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-06-14 15:51:16 -0400

    Added Inventory but not working yet

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -21,16 +21,18 @@ public class Dealership {
             PreparedStatement statement = connection.prepareStatement(allDealershipsQuery);
 
 
-
             ResultSet results = statement.executeQuery();
 
             while (results.next()) {
-                System.out.print("ID: "); System.out.println(results.getString("DealershipID"));
-                System.out.print("Name: "); System.out.println(results.getString("Name"));
-                System.out.print("Address: "); System.out.print(results.getString("Address"));
-                System.out.print("\nPhone: "); System.out.println(results.getString("Phone"));
+                System.out.print("ID: ");
+                System.out.println(results.getString("DealershipID"));
+                System.out.print("Name: ");
+                System.out.println(results.getString("Name"));
+                System.out.print("Address: ");
+                System.out.print(results.getString("Address"));
+                System.out.print("\nPhone: ");
+                System.out.println(results.getString("Phone"));
                 System.out.println("\n");
-
 
 
             }
@@ -38,12 +40,44 @@ public class Dealership {
             results.close();
             statement.close();
             connection.close();
+
+
+            System.out.println("Inventory Data: \n");
+            String allInventoryQuery = "SELECT * FROM Inventory";
+
+
+            Connection connection2;
+            connection2 = DriverManager.getConnection(url, username, password);
+            PreparedStatement statement2 = connection2.prepareStatement(allInventoryQuery);
+
+
+            ResultSet results2 = statement2.executeQuery();
+
+            while (results2.next()) {
+                System.out.print("VIN: ");
+                System.out.println(results2.getString("VIN"));
+                System.out.print("Make: ");
+                System.out.println(results2.getString("VehicleMake"));
+                System.out.print("Model: ");
+                System.out.print(results2.getString("VehicleModel"));
+                System.out.print("\nSold: ");
+                System.out.println(results2.getString("Sold"));
+                System.out.println("\n");
+
+
+            }
+
+
+            results2.close();
+            statement2.close();
+            connection2.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
 
-        }
 
         }
+    }
 }
 
 
