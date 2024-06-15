57f4617539e83a3e27b1a616731d8f35c62619d9
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-06-15 18:52:01 -0400

    Working screens

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -2,79 +2,148 @@ package com.pluralsight;
 
 import java.sql.Connection;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Dealership {
 
+    public static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        String password = args[0];
-        String url = "jdbc:mysql://localhost:3306/CarDealership";
-        String username = "root";
+        boolean home = true;
 
-        try {
-            System.out.println("Dealership Data: \n");
-            String allDealershipsQuery = "SELECT * FROM Dealerships";
+        while (home) {
 
+            System.out.println("Welcome to the Dealership\n Select An Option to Begin: \n(1) View Dealership \n(2) View Vehicles \n(3) Sales Contracts \n(4) Exit");
+            String userInput = myScanner.nextLine();
+            System.out.println("(1) View Dealership \n(2) View Vehicles \n(3) Sales Contracts (4) Lease Contracts (0) Exit");
 
-            Connection connection;
-            connection = DriverManager.getConnection(url, username, password);
-            PreparedStatement statement = connection.prepareStatement(allDealershipsQuery);
+            String password = args[0];
+            String url = "jdbc:mysql://localhost:3306/CarDealership";
+            String username = "root";
 
+            if (userInput.equalsIgnoreCase("1")) {
 
-            ResultSet results = statement.executeQuery();
+                boolean dealership = true;
 
-            while (results.next()) {
-                System.out.print("ID: ");
-                System.out.println(results.getString("DealershipID"));
-                System.out.print("Name: ");
-                System.out.println(results.getString("Name"));
-                System.out.print("Address: ");
-                System.out.print(results.getString("Address"));
-                System.out.print("\nPhone: ");
-                System.out.println(results.getString("Phone"));
-                System.out.println("\n");
+                while (dealership) {
+
+                    try {
+                        System.out.println("Dealership Data: \n");
+                        String allDealershipsQuery = "SELECT * FROM Dealerships";
 
 
+                        Connection connection;
+                        connection = DriverManager.getConnection(url, username, password);
+                        PreparedStatement statement = connection.prepareStatement(allDealershipsQuery);
+
+
+                        ResultSet results = statement.executeQuery();
+
+                        while (results.next()) {
+                            System.out.print("ID: ");
+                            System.out.println(results.getString("DealershipID"));
+                            System.out.print("Name: ");
+                            System.out.println(results.getString("Name"));
+                            System.out.print("Address: ");
+                            System.out.print(results.getString("Address"));
+                            System.out.print("\nPhone: ");
+                            System.out.println(results.getString("Phone"));
+                            System.out.println("\n");
+
+
+                        }
+
+                        results.close();
+                        statement.close();
+                        connection.close();
+
+                        System.out.println("(0) Home");
+                        String dealershipInput = myScanner.nextLine();
+                        if (dealershipInput.equalsIgnoreCase("0")) {
+
+                            dealership = false;
+                        } else {
+
+                            System.out.println("Enter a Valid Option");
+                        }
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+
+
+                    }
+                }
+            } else if (userInput.equalsIgnoreCase("2")) {
+                boolean viewVehicles = true;
+
+                while (viewVehicles) {
+
+                    try {
+                        System.out.println("Vehicles Inventory: \n");
+                        String InventoryQuery = "SELECT * FROM Inventory";
+
+
+                        Connection connection;
+                        connection = DriverManager.getConnection(url, username, password);
+                        PreparedStatement statement = connection.prepareStatement(InventoryQuery);
+
+
+                        ResultSet results = statement.executeQuery();
+
+                        while (results.next()) {
+                            System.out.print("Dealership ID: ");
+                            System.out.println(results.getString("DealershipID"));
+                            System.out.print("VIN: ");
+                            System.out.println(results.getString("VIN"));
+                            System.out.print("Make: ");
+                            System.out.print(results.getString("VehicleMake"));
+                            System.out.print("\nModel: ");
+                            System.out.print(results.getString("VehicleModel"));
+                            System.out.println("\n");
+
+
+                        }
+
+                        results.close();
+                        statement.close();
+                        connection.close();
+
+                        System.out.println("(0) Home");
+                        String inventoryInput = myScanner.nextLine();
+                        if (inventoryInput.equalsIgnoreCase("0")) {
+
+                            viewVehicles = false;
+                        } else {
+
+                            System.out.println("\n Enter a Valid Option\n ");
+                        }
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+
+
+                    }
+
+
+
+
+                }
+
+
+            } else if (userInput.equalsIgnoreCase("3")) {
+
+
+            } else if (userInput.equalsIgnoreCase("4")) {
+
+
+            } else if (userInput.equalsIgnoreCase("0")) {
+
+                System.exit(0);
+            }  else {
+
+                System.out.println("\nEnter a Valid Option\n");
             }
-
-            results.close();
-            statement.close();
-            connection.close();
-
-
-            System.out.println("Inventory Data: \n");
-            String allInventoryQuery = "SELECT * FROM Inventory";
-
-
-            Connection connection2;
-            connection2 = DriverManager.getConnection(url, username, password);
-            PreparedStatement statement2 = connection2.prepareStatement(allInventoryQuery);
-
-
-            ResultSet results2 = statement2.executeQuery();
-
-            while (results2.next()) {
-                System.out.print("VIN: ");
-                System.out.println(results2.getString("VIN"));
-                System.out.print("Make: ");
-                System.out.println(results2.getString("VehicleMake"));
-                System.out.print("Model: ");
-                System.out.print(results2.getString("VehicleModel"));
-                System.out.print("\nSold: ");
-                System.out.println(results2.getString("Sold"));
-                System.out.println("\n");
-
-
-            }
-
-
-            results2.close();
-            statement2.close();
-            connection2.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-
 
         }
     }
